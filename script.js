fa3036bbac66f719f6ab654d3f75d520213e24b6
Vietanh-Vu <vuvietanh993@gmail.com>
2022-08-26 10:54:19 +0700

    upload source code

--- script.js
+++ script.js
@@ -1,98 +1,105 @@
-'use strict';
+'use strict'
 
-// selecting elements
-const player0El = document.querySelector('.player--0')
-const player1El = document.querySelector('.player--1')
-const score0El = document.querySelector('#score--0')
-const score1El = document.querySelector('#score--1')
-const current0El = document.querySelector('#current--0')
-const current1El = document.querySelector('#current--1')
+// select elements
+
+const score0 = document.querySelector('#score--0')
+const score1 = document.querySelector('#score--1')
 const diceEl = document.querySelector('.dice')
-const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
+const btnNew = document.querySelector('.btn--new')
 const btnRule = document.querySelector('.btn--rule')
+const players = document.querySelectorAll('.player')
+const currentScores = document.querySelectorAll('.current-score')
 const modal = document.querySelector('.modal')
 
+// initial values
+let activePlayer, currentScore, score, isWin
 
-// Starting conditions 
-
-let score, currentScore, activePlayer, playing
-
-const init = function () {
-    score = [0,0]
-    currentScore = 0 
+function init () {
     activePlayer = 0
-    playing = true
-    score0El.textContent = 0
-    score1El.textContent = 0
-    current0El.textContent = 0
-    current1El.textContent = 0
-
+    currentScore = 0
+    score = [0,0]
+    isWin = false
+    score0.textContent = 0 
+    score1.textContent = 0 
     diceEl.classList.add('hidden')
-    player0El.classList.remove('player--winner')
-    player1El.classList.remove('player--winner')
-    player0El.classList.add('player--active')
-    player1El.classList.remove('player--active')
+    for (let i of players) {
+        i.classList.remove('player--active')
+        i.classList.remove('player--winner')
+    }
+    for (let i of currentScores) {
+        i.textContent = 0
+    }
+    document.querySelector('.player--0').classList.add('player--active')
 }
 
 init()
 
-let switchPlayer = function() {
+const switchPlayer = function() {
     document.querySelector(`#current--${activePlayer}`).textContent = 0
-    activePlayer = activePlayer === 0 ? 1 : 0
-    currentScore = 0
-    player0El.classList.toggle('player--active')
-    player1El.classList.toggle('player--active')
+    currentScore = 0 
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    activePlayer = activePlayer == 0 ? 1 : 0 
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
 }
 
-// Rolling dice 
-
-btnRoll.addEventListener('click', () =>{
-    if (playing) {
-        //1. Generating a random dice roll 
-        const dice = Math.floor(Math.random() * 6 + 1)
-        //2. Display dice 
+// roll dice 
+btnRoll.addEventListener('click', () => {
+    if (isWin === false) {
+        let dice = Math.floor(Math.random() * 6 + 1)
         diceEl.classList.remove('hidden')
         diceEl.src = `dice-${dice}.png`
-    
-        //3. Check for rolled 1 
+        
         if (dice !== 1) {
-            // add dice to current score 
-            currentScore += dice
+            currentScore += dice 
             document.querySelector(`#current--${activePlayer}`).textContent = currentScore
         } else {
-            // switch to new player 
             switchPlayer()
         }
     }
 })
 
+// hold button
 btnHold.addEventListener('click', () => {
-    if (playing) {
-        // 1. Add current score to active player's score 
+    if (isWin === false) {
         score[activePlayer] += currentScore
         document.querySelector(`#score--${activePlayer}`).textContent = score[activePlayer]
-        // 2. Check if player's score is >= 100
-        if (score[activePlayer] >= 100){
-            // Finish the game
-            playing = false
-            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        if (score[activePlayer] >= 100) {
+            document.querySelector(`.player--${activePlayer}`).classList.add(`player--winner`)
+            isWin = true
         } else {
-            // switch to next player 
             switchPlayer()
         }
     }
 })
 
-// new game 
-btnNew.addEventListener('click', init)
-
-// game rule 
-btnRule.addEventListener('click', () => {
-    modal.style.display = 'flex'
+//2 way to play new game
+btnNew.addEventListener('click', () => {
+    init()
 })
 
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key === 'Enter') {
+        init()
+    }
+})
+
+// rule button
+btnRule.addEventListener('click', () => {
+   modal.style.display = 'flex'
+})
+
+// 2 way to close modal rule
 modal.addEventListener('click', () => {
     modal.style.display = 'none'
 })
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        modal.style.display = 'none'
+    }
+})
+
+
+
